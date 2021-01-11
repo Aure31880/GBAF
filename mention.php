eb71b052111dcfa3918c2c1fcb312c7cd7194c98
Aure31880 <barbotin.aurelien@wanadoo.fr>
2021-01-11 15:28:19 +0100

    moddif mentions

--- mention.php
+++ mention.php
@@ -26,25 +26,6 @@
             Les visiteurs de votre site web peuvent télécharger et extraire des données de localisation depuis ces
             images.</p>
 
-            <h3>Formulaires de contact</h3>
-            <h3>Cookies</h3>
-            <p>Si vous déposez un commentaire sur notre site, il vous sera proposé d’enregistrer votre nom, adresse de
-            messagerie et site web dans des cookies. C’est uniquement pour votre confort afin de ne pas avoir à saisir
-            ces informations si vous déposez un autre commentaire plus tard. Ces cookies expirent au bout d’un an.</p>
-
-            <p>Si vous vous rendez sur la page de connexion, un cookie temporaire sera créé afin de déterminer si votre
-            navigateur accepte les cookies. Il ne contient pas de données personnelles et sera supprimé automatiquement
-            à la fermeture de votre navigateur.</p>
-
-            <p>Lorsque vous vous connecterez, nous mettrons en place un certain nombre de cookies pour enregistrer vos
-            informations de connexion et vos préférences d’écran. La durée de vie d’un cookie de connexion est de deux
-            jours, celle d’un cookie d’option d’écran est d’un an. Si vous cochez « Se souvenir de moi », votre cookie
-            de connexion sera conservé pendant deux semaines. Si vous vous déconnectez de votre compte, le cookie de
-            connexion sera effacé.</p>
-
-            </p>En modifiant ou en publiant une publication, un cookie supplémentaire sera enregistré dans votre navigateur.
-            Ce cookie ne comprend aucune donnée personnelle. Il indique simplement l’ID de la publication que vous venez
-            de modifier. Il expire au bout d’un jour.</p>
 
             <h3>Contenu embarqué depuis d’autres sites</h3>
             <p>Les articles de ce site peuvent inclure des contenus intégrés (par exemple des vidéos, images, articles…).
